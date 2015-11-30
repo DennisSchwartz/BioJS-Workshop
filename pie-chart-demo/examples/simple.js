ef3bb2552dc2edb36ebb0d6cb2075b3ac2bcd72c
Dennis Schwartz <schwartz_dennis@web.de>
2015-11-30 16:07:48 +0000

    Deleted everything for new try

--- pie-chart-demo/examples/simple.js
+++
@@ -1,7 +0,0 @@
-// if you don't specify a html file, the sniper will generate a div with id "rootDiv"
-var app = require("pie-chart-demo");
-
-var instance = new app({
-    el: rootDiv,
-    sequence: "CTCTGAGCCAGGAGACATTTTCAGGC"
-});
