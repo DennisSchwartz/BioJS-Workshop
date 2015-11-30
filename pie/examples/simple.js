0d561be2094fecbfaa3ffcf7d99fb1df0886741b
Dennis Schwartz <schwartz_dennis@web.de>
2015-11-30 19:01:41 +0000

    Restructured the combination example into a single folder

--- pie/examples/simple.js
+++
@@ -1,9 +0,0 @@
-// if you don't specify a html file, the sniper will generate a div with id "rootDiv"
-
-var app = require("biojs-vis-gcpiechart");
-
-
-var instance = new app({
-    el: rootDiv,
-    sequence: "TGACGATGATCAGAT"
-});
